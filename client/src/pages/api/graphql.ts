e74165243cab6b5d157809dca026ba0575ec54ef
SketchTurnerrr <nareknarek45@gmail.com>
2022-08-11 11:01:59 +0300

    setup 3/?

--- client/src/pages/api/graphql.ts
+++ client/src/pages/api/graphql.ts
@@ -1,7 +1,12 @@
 import { createServer } from '@graphql-yoga/node';
+import { readFileSync } from 'fs';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { join } from 'path';
 import {schema} from '../../../graphql/schema'
 
+const typeDefs = readFileSync(join(process.cwd(), "schema.ts"), {
+  encoding: 'utf-8'
+})
 
 const server = createServer<{
   req: NextApiRequest;
